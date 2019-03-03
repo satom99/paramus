9893903d68fc0d7681923cebb19c11e69bb995c8
satom99 <santitm99@gmail.com>
2019-03-03 14:33:03 +0100

    Hex release

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Paramus
 
+[![License](https://img.shields.io/github/license/satom99/paramus.svg)](https://github.com/satom99/paramus/blob/master/LICENSE)
+[![Hex](http://img.shields.io/hexpm/v/paramus.svg?style=flat)](https://hex.pm/packages/paramus)
+[![Donate](https://img.shields.io/badge/donate-PayPal-yellow.svg)](https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=JKKHNZF6RAKDA&item_name=paramus&currency_code=USD)
+
 Parameter validation for [Phoenix](https://github.com/phoenixframework/phoenix).
 
 ### Example
--- mix.exs
+++ mix.exs
@@ -4,10 +4,12 @@ defmodule Paramus.MixProject do
   def project do
     [
       app: :paramus,
-      version: "0.1.0",
+      version: "1.0.0",
       elixir: "~> 1.2",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
+      description: "Parameter validation for Phoenix.",
+      package: package(),
       deps: deps()
     ]
   end
@@ -16,6 +18,13 @@ defmodule Paramus.MixProject do
     [
       {:ecto, ">= 2.0.0"},
       {:phoenix, ">= 0.6.0"}
+    ]
+  end
+
+  defp package do
+    [
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/satom99/paramus"}
     ]
   end
 end
